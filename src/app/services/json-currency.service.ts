2edeedab10772d8667caac10c5dabe0234b3991f
drmrhdt <drmrhdt@gmail.com>
2020-11-08 11:01:37 +0300

    refactor: delete unused imports

--- src/app/services/json-currency.service.ts
+++ src/app/services/json-currency.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { GenericCurrency, RowValuteData, Valute } from '../models';
